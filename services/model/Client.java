db4218e995430a7c686eb03daaa721d88de01879
Gorbatenko <gorvit.dp@gmail.com>
2017-11-29 23:40:12 +0200

    clientRefactor

--- services/model/Client.java
+++ services/model/Client.java
@@ -1,16 +1,22 @@
 package BigProject.services.model;
 
 public class Client {
+    private int id;
     private String name;
     private String surname;
     private String email;
     private int phone;
 
-    public Client(){
+    public Client(int id, String name, String surname, String email, int phone){
+        this.id = id;
         this.name = name;
         this.surname = surname;
         this.email = email;
         this.phone = phone;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public String getName() {
@@ -27,5 +33,10 @@ public class Client {
 
     public int getPhone() {
         return phone;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + ", " + name + ", " + surname + ", " + email + ", " + phone + "]";
     }
 }
